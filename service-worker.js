fd7943ca55508fb1e96a0573b6851ffdf3af53d7
Kreshel-commits <kresshel@gmail.com>
2026-09-26 08:51:36 +0300

    Publish Poruch intake restoration fix from 483d373

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'poruch-static-shell-ba0f6da8d5db';
+const CACHE = 'poruch-static-shell-44dd04798d78';
 const BASE = '/poruch-preview';
 const ROUTES = [
   '/', '/situation', '/clarify', '/import-document', '/next-step',
